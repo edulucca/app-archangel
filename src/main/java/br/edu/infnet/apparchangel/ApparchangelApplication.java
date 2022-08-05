042125d1c8993d165162cebab309d4309bb483d6
Eduardo de Lucca <eduardo.luccati@gmail.com>
2022-08-04 23:33:42 -0300

    Initial Commit

---
+++ src/main/java/br/edu/infnet/apparchangel/ApparchangelApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apparchangel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparchangelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApparchangelApplication.class, args);
+	}
+
+}

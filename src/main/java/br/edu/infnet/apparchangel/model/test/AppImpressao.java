5bc0b9b7ff06f9584873b02393ad47089a57d356
Eduardo De Lucca Santos Santana <eduardo.s.santana@ericssoninovacao.com.br>
2022-08-14 19:27:50 -0300

    Feature 4 Concluída

--- src/main/java/br/edu/infnet/apparchangel/model/test/AppImpressao.java
+++ src/main/java/br/edu/infnet/apparchangel/model/test/AppImpressao.java
@@ -4,6 +4,7 @@ import br.edu.infnet.apparchangel.interfaces.IPrinter;
 import br.edu.infnet.apparchangel.model.domain.Crise;
 
 public class AppImpressao {
+    //Feature 4 - Finalizada
     public static void relatorio(IPrinter printer, String mensagem){
         System.out.println();
         System.out.println(mensagem);
